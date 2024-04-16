5059b07f5beed3357ecdafeef6f96a409875fceb
Giovanna Neves Rodrigues <giovanna.neves2007@gmail.com>
2024-04-16 18:48:32 -0300

    V2.0.0
    Projeto Finalizado

--- script.js
+++ script.js
@@ -10,8 +10,41 @@ function updateTime() {
   
     var timeString = hours + ':' + minutes + ':' + seconds;
     document.getElementById('clock').innerText = timeString;
-  }
+}
   
   setInterval(updateTime, 1000);
   
   updateTime();
+
+function descobrir() {
+  var fraseElemento = document.getElementById("mudar"); 
+  var frase = fraseElemento.textContent; 
+  frase = frase.replace("Nossos cursos", "Nossas oportunidades para você");
+  fraseElemento.textContent = frase;
+}
+
+//Slideshow
+
+var slide = document.querySelector(".slideshow");
+var imagens = [
+  './assets/etec_taboao.png',
+  './assets/etec-taboao-drone.jpg',
+  './assets/etectaboaodaserra.jpg'
+];
+
+var time = 2000
+var count = 0
+
+function moveSlideShow() {
+  slide.src = imagens[count]
+
+  if (count < imagens.length - 1) {
+    count++
+  } else {
+    count = 0
+  }
+
+  setTimeout("moveSlideShow()", time)
+}
+
+window.onload = moveSlideShow
